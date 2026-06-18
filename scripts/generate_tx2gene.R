0c0b1b7af5b97d70f7beff7a380abee76c429102
Anna <annabertelle@usp.br>
2026-06-18 19:38:00 -0300

    Update generate_tx2gene.R

--- scripts/generate_tx2gene.R
+++ scripts/generate_tx2gene.R
@@ -1,13 +1,16 @@
-library(GenomicFeatures)
+library(snakemake)
 
-# Ajustado para a nova versão exigida pelo Bioconductor
+#A função makeTxDbFromGFF pega o caminho GTF, extrai as informações (genes, transcritos, éxons,etc) e organiza em um objeto TxDB (transcript database)
+
 txdb <- txdbmaker::makeTxDbFromGFF(
-    "reference/annotation.gtf",
+    snakemake@input[[1]],
     format="gtf"
 )
 
+#extrai todos os IDs de transcritos no banco. Para ver as outras chaves, use keytypes(txdb)
 k <- keys(txdb, keytype="TXNAME")
 
+#seleciona o gene correspondente a cada transcript_ID
 tx2gene <- select(
     txdb,
     keys=k,
@@ -17,6 +20,6 @@ tx2gene <- select(
 
 write.csv(
     tx2gene,
-    "reference/tx2gene.csv",
+    snakemake@output[[1]],
     row.names=FALSE
 )
